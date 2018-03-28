ef2a958a95e5cb18ec91dd023f5a2ee203bc4668
youthfulacts <32503015+youthfulacts@users.noreply.github.com>
2018-03-27 16:58:37 -1000

    Continue

---
+++ BlockBreaker/Assets/Scripts/Music_Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Player : MonoBehaviour {
+    static Music_Player instance = null;
+	// Use this for initialization
+	void Start () {
+        if (instance != null) {
+            Destroy (gameObject);
+            print ("Duplicate music player self-destruction!");
+        } else {
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
